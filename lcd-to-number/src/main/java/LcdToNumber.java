0e955eeda97026c0f4831e3af965544c610ca1a8
Daniel Strassenburg <daniel.strassenburg@gmx.net>
2012-01-04 19:32:19 +0100

    finished today...

--- lcd-to-number/src/main/java/LcdToNumber.java
+++ lcd-to-number/src/main/java/LcdToNumber.java
@@ -1,3 +1,9 @@
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Daniel
@@ -6,4 +12,52 @@
  * To change this template use File | Settings | File Templates.
  */
 public class LcdToNumber {
+
+  //static int[] numbers = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  public static int parseDigit(String lcdDigit) {
+    return 1;
+  }
+
+  protected static List<Integer> parseLine(String line, int lineNr) {
+
+    switch (lineNr) {
+      case 1 : return parseFirstLine(line);
+    }
+    return null;
+  }
+
+  private static List<Integer> parseFirstLine(String line) {
+    if (line.equals("   ")) {
+      return Arrays.asList(1, 4);
+    }
+    if (line.equals(" _ ")) {
+      return Arrays.asList(0, 2, 3, 5, 6, 7, 8, 9);
+    }
+    throw new IllegalArgumentException("Character " + line + " unexpected in first line.");
+  }
+
+  private static List<Integer> parseSecondLine(String line) {
+    List<Integer> numbersList = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+    switch (line.charAt(0)) {
+      case ' ':
+        numbersList.removeAll(Arrays.asList(0, 4, 5, 6, 8, 9));
+        break;
+      case '|': numbersList.removeAll(Arrays.asList(1,2,3,7));
+    }
+    switch (line.charAt(1)) {
+      case ' ':
+        numbersList.removeAll(Arrays.asList(2,3,4,5,6,8,9));
+        break;
+      case '_': numbersList.removeAll(Arrays.asList(0,1,7));
+    }
+    switch (line.charAt(3)) {
+      case ' ':
+        numbersList.removeAll(Arrays.asList(1,2,3,4,7,8,9));
+        break;
+      case '|': numbersList.removeAll(Arrays.asList(5,6,0));
+    }
+
+    return numbersList;
+  }
 }
--- lcd-to-number/src/main/java/LcdToNumberTest.java
+++ lcd-to-number/src/main/java/LcdToNumberTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Daniel
@@ -12,5 +21,26 @@ public class LcdToNumberTest {
   "|" +
   "|" +
   " ";
+
+  @Test
+  public void testParseDigit_1() {
+    assertThat(1, is(LcdToNumber.parseDigit(ONE)));
+  }
+
+  @Test
+  public void testParseFirstLine_1() {
+    assertThat(Arrays.asList(0,2,3,5,6,7,8,9), is(equalTo(LcdToNumber.parseLine(" _ ", 1))));
+    assertThat(Arrays.asList(1,4), is(equalTo(LcdToNumber.parseLine("   ", 1))));
+  }
+
+  public void testParseSecondLine_2() {
+    // check
+    // '  |' ->
+    // '  _|'
+    // '|_|'
+    // ' _ '
+    // '|_ '
+    // '|  '
+  }
 
 }
