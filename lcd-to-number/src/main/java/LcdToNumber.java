290691a86825b7301de942efeaff6793a290e4e5
Daniel Strassenburg <daniel.strassenburg@gmx.net>
2012-01-04 18:49:53 +0100

    initial commit

---
+++ lcd-to-number/src/main/java/LcdToNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel
+ * Date: 04.01.12
+ * Time: 18:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class LcdToNumber {
+}
---
+++ lcd-to-number/src/main/java/LcdToNumberTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel
+ * Date: 04.01.12
+ * Time: 18:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class LcdToNumberTest {
+
+  private static final String ONE =
+  " " +
+  "|" +
+  "|" +
+  " ";
+
+}
